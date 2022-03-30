67b8ec9cc830bdd33000492d6be76c260e5c9d2f
samuel legese <samuellegese@samuels-MacBook-Air.local>
2022-03-30 10:46:52 +0300

    inital commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+        <Nuxt />
+    </v-main>
+    <!-- <v-footer fixed="false" class="align-self-center" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer> -->
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
